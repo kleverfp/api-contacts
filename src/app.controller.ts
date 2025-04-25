840ef3eb57100dab45eefde524180bc617a96a22
Klever Ferreira <kleverfp@hotmail.com>
2025-04-25 12:26:59 -0300

    feat: get version controller method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getVersion(): any {
     return this.appService.getVersion();
   }
 }
